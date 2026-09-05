b28d62ae04f46128893d347cbf1e2f43a6baf2d4
Arun Rajkumar <connectwitharunrk@gmail.com>
2026-09-05 20:32:49 +0530

    sum of fifth powers of the first n natural numbers

--- GFG-Pattern/SumOfNaturalNumberCubes.kt
+++ GFG-Pattern/SumOfNaturalNumberCubes.kt
@@ -1,4 +1,5 @@
 // https://www.geeksforgeeks.org/problems/sum-of-first-n-terms5843/1
+// https://www.geeksforgeeks.org/problems/sum-of-fifth-powers-of-the-first-n-natural-numbers3415/1
 // Sum of Natural Number Cubes
 
 import kotlin.math.pow
@@ -10,6 +11,21 @@ fun main() {
 	
 	for(i in 1..n) {
 		sum += i.toDouble().pow(3.toDouble()).toInt()
+	}
+	
+	println(sum)
+
+}
+
+import kotlin.math.pow
+
+fun main() {
+
+	val n = 5
+	var sum = 0
+	
+	for(i in 1..n) {
+		sum += i.toDouble().pow(5.toDouble()).toInt()
 	}
 	
 	println(sum)
